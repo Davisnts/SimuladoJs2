ee577dca6d88a824f4e43db37de2de40ae8e93e6
Davisnts <davitpontas@gmail.com>
2022-11-22 20:27:06 -0300

    Tamo

--- noticias.js
+++ noticias.js
@@ -1,1 +1,32 @@
-console.log('Test')
+class Jornalista{
+    constructor(foto,nome){
+        this.foto = foto;
+        this.nome = nome;
+    }
+    analisarNoticias(){
+        console.log('Analisando a Noticia');
+    }
+}
+class Noticia extends Jornalista{
+    constructor(foto,nome,artigo){
+        super(foto,nome);
+        this.artigo = artigo;
+    }
+    analisarNoticias(){
+        super.analisarNoticias();
+    }
+    inserirVideo(){
+        console.log('Inserindo Video');
+    }
+}
+
+const Lula = new Noticia('Jair Bolsonaro' ,'milicia.jpg',157);
+const Bolsonaro = new Noticia('Luiz Inacio','calango.jpg',171);
+const Eneas = new Noticia('Bomba','Nuclear',5656);
+const GrandePerda = new Jornalista('Ricardo Eugênio Boechat','Ricardo.jpg')
+const Ancora = new Jornalista('Willian Bonner','Bonner.png')
+const Encontro = new Jornalista('Fatima Bernardes','Tvglobinho.webp')
+
+var Noticias= [Lula,Bolsonaro,Eneas];
+var Jornalistas = [GrandePerda,Ancora,Encontro];
+
